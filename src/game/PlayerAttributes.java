fcc22ae3f764527042df000edcb55bfdd50d4202
Pedro da Luz <pedro.luz@hotmail.com.br>
2017-02-15 00:06:58 -0200

    Método para setar atributos criado

--- src/game/PlayerAttributes.java
+++ src/game/PlayerAttributes.java
@@ -1,6 +1,9 @@
 package game;
+import java.io.BufferedReader;
+import java.io.FileInputStream;
 // JAVA LIBRARIES
 import java.io.IOException;
+import java.io.InputStreamReader;
 
 /**
  * Created by Alex, Pedro & Igor on Jan/2017.
@@ -30,7 +33,40 @@ public class PlayerAttributes extends Attributes {
 	}
 	
 	public void setAttributes(int id) throws IOException {
-		id++;
+		String gamePath = System.getProperty("user.dir");
+		String dataPath = gamePath + System.getProperty("file.separator") + "data" + System.getProperty("file.separator") + "character";
+		String playerPath = dataPath + System.getProperty("file.separator") + "enemy.csv";
+		
+		BufferedReader readPlayerFile = new BufferedReader(
+				new InputStreamReader(new FileInputStream(playerPath), "UTF-8"));
+		
+		String line = readPlayerFile.readLine(); // First line doesn't count
+		int currentID;
+		int strength;
+		int intelligence;
+		int agility;
+		int stamina;
+		int spirit;
+		int social;
+		
+		do {
+			line = readPlayerFile.readLine();
+			currentID = new Integer(line.split(";")[0]);
+			strength = new Integer(line.split(";")[2]);
+			intelligence = new Integer(line.split(";")[3]);
+			agility = new Integer(line.split(";")[4]);
+			stamina = new Integer(line.split(";")[5]);
+			spirit = new Integer(line.split(";")[6]);
+			social = new Integer(line.split(";")[7]);
+		} while(currentID != id);
+		readPlayerFile.close();
+		
+		setStrength(strength);
+		setIntelligence(intelligence);
+		setAgility(agility);
+		setStamina(stamina);
+		setSpirit(spirit);
+		setSocial(social);
 	}
 	
 	private int social;
