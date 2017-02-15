61da418a6c862502f3ddab2a9aac48b15c351c6c
Pedro da Luz <pedro.luz@hotmail.com.br>
2017-02-15 15:59:18 -0200

    Classe para instanciar equipamento criada

--- src/game/Item.java
+++ src/game/Item.java
@@ -12,7 +12,7 @@ public class Item {
 		setup(id);
 	}
 	
-	protected void setup(int id) throws IOException {
+	private void setup(int id) throws IOException {
 		String gamePath = System.getProperty("user.dir");
 		String dataPath = gamePath + System.getProperty("file.separator") + "data" + System.getProperty("file.separator") + "item";
 		String itensFile = dataPath + System.getProperty("file.separator") + "itens.csv";
@@ -41,6 +41,38 @@ public class Item {
 		setPrice(price);
 		setWeight(weight);
 		setEquipment_id(equipment_id);
+	}
+	
+	protected void setupEq(int id) throws IOException {
+		String gamePath = System.getProperty("user.dir");
+		String dataPath = gamePath + System.getProperty("file.separator") + "data" + System.getProperty("file.separator") + "item";
+		String itensFile = dataPath + System.getProperty("file.separator") + "itens.csv";
+		
+		BufferedReader readItensFile = new BufferedReader(
+				new InputStreamReader(new FileInputStream(itensFile), "UTF-8"));
+		
+		String line = readItensFile.readLine(); // First line doesn't count
+		int currentID;
+		String name;
+		double price;
+		double weight;
+		int item_id;
+		
+		do {
+			line = readItensFile.readLine();
+			currentID = new Integer(line.split(";")[4]);
+			name = new String(line.split(";")[1]);
+			price = new Double(line.split(";")[2]);
+			weight = new Double(line.split(";")[3]);
+			item_id = new Integer(line.split(";")[0]);
+		} while(currentID != id);
+		readItensFile.close();
+		
+		setName(name);
+		setPrice(price);
+		setWeight(weight);
+		setEquipment_id(id);
+		setItem_id(item_id);
 	}
 	
 	public String getName() {
@@ -74,9 +106,19 @@ public class Item {
 	public void setEquipment_id(int equipment_id) {
 		this.equipment_id = equipment_id;
 	}
+	
+	public int getItem_id() {
+		return item_id;
+	}
+
+	public void setItem_id(int item_id) {
+		this.item_id = item_id;
+	}
 
 	protected String name;
 	protected double price;
 	protected double weight;
+	protected int item_id;
+	
 	private int equipment_id;
 }
